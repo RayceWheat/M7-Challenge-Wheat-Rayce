3a64b1d8ecfdf2658b8b3d703003d72364dd296d
RayceWheat <RayceWheat@gmail.com>
2022-08-05 14:57:12 -0500

    musticstore recommendations

--- musicstore-recommendations/src/main/java/com/trilogyed/MusicStoreRecommendations/repository/AlbumRecommendationRepository.java
+++ musicstore-recommendations/src/main/java/com/trilogyed/MusicStoreRecommendations/repository/AlbumRecommendationRepository.java
@@ -1,2 +1,9 @@
-package com.trilogyed.MusicStoreRecommendations.repository;public interface AlbumRecommendationRepository {
+package com.trilogyed.MusicStoreRecommendations.repository;
+
+import com.trilogyed.MusicStoreRecommendations.model.AlbumRecommendation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRecommendationRepository extends JpaRepository<AlbumRecommendation, Integer> {
 }
